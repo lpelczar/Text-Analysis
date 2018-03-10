83d8582b3a21b1ad74981b1f84164b92536049b8
lpelczar <lukepelczar@gmail.com>
2018-03-10 16:35:15 +0100

    Add file content class with empty methods

---
+++ src/main/java/FileContent.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public class FileContent implements IterableText {
+
+    public Iterator<String> charIterator() {
+        return null;
+    }
+
+    public Iterator<String> wordIterator() {
+        return null;
+    }
+
+    public String getFilename() {
+        return null;
+    }
+}
