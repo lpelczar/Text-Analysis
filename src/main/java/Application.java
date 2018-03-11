23e3bc7db36a42ac21c6d191a461f790ba6fd08e
lpelczar <lukepelczar@gmail.com>
2018-03-11 11:42:07 +0100

    Add initialization of root and invocation of start method in app class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,5 +2,8 @@ public class Application {
 
     public static void main(String... args) {
 
+        RootView rootView = new RootView();
+        RootController rootController = new RootController(args, rootView);
+        rootController.start();
     }
 }
