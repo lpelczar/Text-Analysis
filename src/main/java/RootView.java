61cd30c54c97bce8b801a0822272c97fbc6d0475
lpelczar <lukepelczar@gmail.com>
2018-03-11 12:25:25 +0100

    Add root view class

---
+++ src/main/java/RootView.java
@@ -0,0 +1,6 @@
+public class RootView {
+
+    public void displayNoFileProvidedMessage() {
+        System.out.println("You need to provide file in console argument like 'java Application filename.txt'");
+    }
+}
