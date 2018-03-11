632d9df053c168dcdb874db6d84e65d9088d226c
lpelczar <lukepelczar@gmail.com>
2018-03-11 12:26:35 +0100

    Add statistical analysis class with constructor

---
+++ src/main/java/StatisticalAnalysis.java
@@ -0,0 +1,10 @@
+import java.util.Iterator;
+
+public class StatisticalAnalysis {
+
+    private Iterator<String> iterator;
+
+    public StatisticalAnalysis(Iterator<String> iterator)  {
+        this.iterator = iterator;
+    }
+}
