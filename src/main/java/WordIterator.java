87e91542c16dbbef85f6e93144d4917db8549c25
lpelczar <lukepelczar@gmail.com>
2018-03-11 10:46:57 +0100

    Add word iterator class with empty methods

---
+++ src/main/java/WordIterator.java
@@ -0,0 +1,12 @@
+import java.util.Iterator;
+
+public class WordIterator implements Iterator {
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public Object next() {
+        return null;
+    }
+}
