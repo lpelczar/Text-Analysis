794029a73afe915787016c8d5083dab0f72c5470
lpelczar <lukepelczar@gmail.com>
2018-03-11 15:12:27 +0100

    Modify size and dictionary size methods

--- src/main/java/com/codecool/textanalysis/services/StatisticalAnalysis.java
+++ src/main/java/com/codecool/textanalysis/services/StatisticalAnalysis.java
@@ -21,23 +21,18 @@ public class StatisticalAnalysis {
     }
 
     public int dictionarySize() {
-        Set<String> set = new HashSet<>();
-        Set<String> duplicateSet = new HashSet<>();
-
-        while (iterator.hasNext()) {
-            if (!set.add(iterator.next())) {
-                duplicateSet.add(iterator.next());
-            }
-        }
-        set.removeAll(duplicateSet);
-        return set.size();
-    }
-
-    public int size() {
         Set<String> elements = new HashSet<>();
         while (iterator.hasNext()) {
             elements.add(iterator.next());
         }
         return elements.size();
+    }
+
+    public int size() {
+        int totalNumber = 0;
+        while (iterator.hasNext()) {
+            totalNumber++;
+        }
+        return totalNumber;
     }
 }
