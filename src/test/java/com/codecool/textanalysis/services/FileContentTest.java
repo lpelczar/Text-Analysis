9057d942df61fff46a2f19e5062718bc05c324d4
lpelczar <lukepelczar@gmail.com>
2018-03-12 13:00:09 +0100

    Add tests checking wordinterator and chariterator methods

--- src/test/java/com/codecool/textanalysis/services/FileContentTest.java
+++ src/test/java/com/codecool/textanalysis/services/FileContentTest.java
@@ -3,6 +3,7 @@ package com.codecool.textanalysis.services;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileContentTest {
@@ -18,5 +19,17 @@ class FileContentTest {
     void whenInstantiatedThenFilepathIsSet() {
         String expected = "testShort.txt";
         assertEquals(expected, fileContent.getFilename());
+    }
+
+    @Test
+    void whenCharIteratorCalledThenReturnCharIteratorClassObject() {
+        assertThat(this.fileContent.charIterator())
+                .isOfAnyClassIn(CharIterator.class);
+    }
+
+    @Test
+    void whenWordIteratorCalledThenReturnWordIteratorClassObject() {
+        assertThat(this.fileContent.wordIterator())
+                .isOfAnyClassIn(WordIterator.class);
     }
 }
