9eeb1e70053365c73b37de68818802d317c383e9
lpelczar <lukepelczar@gmail.com>
2018-03-12 12:50:29 +0100

    Add test checking filename field in file content class

---
+++ src/test/java/com/codecool/textanalysis/services/FileContentTest.java
@@ -0,0 +1,22 @@
+package com.codecool.textanalysis.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FileContentTest {
+
+    private FileContent fileContent;
+
+    @BeforeEach
+    void before() {
+        this.fileContent = new FileContent("testShort.txt");
+    }
+
+    @Test
+    void whenInstantiatedThenFilepathIsSet() {
+        String expected = "testShort.txt";
+        assertEquals(expected, fileContent.getFilename());
+    }
+}
